ebbba46375bdee5c81dbbebecda95f91cbde9f9c
Saul Echeverri <shecheve@bancolombia.com.co>
2025-09-17 12:09:07 -0500

    🎉(feat):Initial commit

---
+++ src/main/java/com/education/course_management_spring_boot/CourseManagementSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.education.course_management_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagementSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManagementSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/education/course_management_spring_boot/CourseManagementSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.education.course_management_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
